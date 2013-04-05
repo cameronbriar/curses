9580a897c159d0b36687e194d295d496c5e2fbaa
Cameron Briar <cameronbriar@mail.fresnostate.edu>
2013-04-04 22:07:28 -0700

    key board map

---
+++ key.py
@@ -0,0 +1,26 @@
+"""
+Returns a dictionary of the keyboard 
+mapped to its ord() value.
+
+string DATA
+    ascii_letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    ascii_lowercase = 'abcdefghijklmnopqrstuvwxyz'
+    digits = '0123456789'
+    hexdigits = '0123456789abcdefABCDEF'
+    letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    octdigits = '01234567'
+    printable = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTU...
+    punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+    uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    whitespace = '\t\n\x0b\x0c\r '
+"""
+import string
+
+class Key():
+
+    def __init__(self):
+        
+         self.key = {}
+         
+         for k in string.printable:
+             self.key[k] = ord(k)
