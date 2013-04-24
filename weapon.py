26800579b77086825f14b7b5d1ad7939419dabc5
Cameron Briar <cameronbriar@mail.fresnostate.edu>
2013-04-23 18:12:40 -0700

    block fix

--- weapon.py
+++ weapon.py
@@ -3,7 +3,7 @@ class Weapon:
         return
 
     def block(self, image="|", length=10, width=1):
-        return ''.join([image * length + '\n'] * width)[:-1]
+        return '\n'.join([image * length] * width)
 
     def cube(self, image="|", size=5):
-        return ''.join([image * size + '\n'] * ((size/2)+1))[:-1]
+        return '\n'.join([image * size] * ((size/2)+1))
